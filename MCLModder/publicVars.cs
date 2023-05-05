c6b54d51579854a2c484360ee1a16a4e83ddb948
emanm <emanm@EMANCOMPUTER>
2023-05-05 14:47:53 +0300

    Add project files.

---
+++ MCLModder/publicVars.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using MCLModder;
+using System.IO;
+using System.Security.Policy;
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json.Converters;
+
+namespace MCLModder
+{
+    public class publicVars
+    {
+        public string gameFilesPath = "C:\\XboxGames\\Minecraft Legends - Windows";
+        public string storedDataFilePath;
+    }
+}
---
+++ MCLModder/storeData.cs
@@ -0,0 +1,16 @@
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json.Converters;
+using MCLModder;
+using System.IO;
+using System.Text;
+
+namespace MCLModder
+{
+    
+    public class storeFuncations
+    {
+        
+    }
+}
