351ac035346328060264f2f2b445481ebda567c1
Mohammed85 <100155245+MohammedGamer85@users.noreply.github.com>
2023-05-07 19:38:56 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+Console.WriteLine("I wish this will work");
+
+
---
+++ Vars.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+using System.Reflection.Metadata.Ecma335;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using MCLModder;
+
+namespace MCLModder
+{
+    class Vars
+    {
+        
+    }
+}
